9da63d74c1c354d98a733ca83eed00f5c5e80ab9
Katerin <ckaterinarellano@gmail.com>
2023-05-23 09:57:48 -0500

    finish proyect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "./componentes/Container";
+import Header from "./componentes/Header";
+import Welcome from "./Welcome";
+import CharacterContainer from "./componentes/CharacterContainer";
+import { useState } from "react";
 
 function App() {
+
+  const [character, setCharacter] = useState('');
+
+  const reqApi = async () => {
+    const api = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=3');
+    const frases = await api.json();
+    setCharacter(frases);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header/>
+      {!character ?  <Welcome reqApi={reqApi}/>
+        : <CharacterContainer characters={character} reqApi={reqApi}/>
+      }
+    </Container>
   );
 }
 
